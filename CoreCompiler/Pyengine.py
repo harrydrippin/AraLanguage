a30d64d90f623642a598e0288caf82d331aca3eb
Seunghwan Hong <hj332921@naver.com>
2015-05-03 18:46:17 +0900

    Pyengine.py - 넣기, 출력하기 처리 완료

--- CoreCompiler/Pyengine.py
+++ CoreCompiler/Pyengine.py
@@ -1,7 +1,23 @@
 __author__ = 'Seunghwan Hong'
 
-def convert(list) :
-    return list;
+def convert(araCode):
+    result = []
+    for i in range(0, len(araCode)):
+        data = araCode[i]
+
+        r_push = data.find("넣기")
+        r_print = data.find("출력하기")
+
+        if r_push != -1:
+            a = data.replace("에 ", " = ").replace("을 넣기", "").replace("를 넣기", "")
+            result.append(a)
+        elif r_print != -1:
+            a = data.replace("을 출력하기", '').replace("를 출력하기", '').replace("\n", '')
+            b = "print(" + a + ")\n"
+            result.append(b)
+        else:
+            result.append(data)
+    return result
 
 if __name__ == "__main__":
     import sys
